fd7390edd4dba2cbeab30b4d5f2b3ea672514dbf
Ryan Hoerr <rhoerr@users.noreply.github.com>
2025-04-22 20:54:52 -0400

    chore: fix supported version services and dates for Mage-OS 1.1.1

--- supported-version/src/kind/get-currently-supported.spec.ts
+++ supported-version/src/kind/get-currently-supported.spec.ts
@@ -110,7 +110,7 @@ describe('getCurrentlySupportedVersions for mage-os', () => {
         ['2024-07-18T00:00:01Z', 'Release of 1.0.2', [
             'mage-os/project-community-edition:1.0.2',
         ]],
-        ['2025-04-21T00:00:01Z', 'Release of 1.1.1', [
+        ['2025-04-22T00:00:01Z', 'Release of 1.1.1', [
             'mage-os/project-community-edition:1.1.1',
         ]],
         ])(
