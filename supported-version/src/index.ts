46b6d4539d3fac82763c81564f9a232a95ce00c2
Damien Retzinger <damienwebdev@gmail.com>
2026-05-02 10:37:14 -0400

    feat(supported-version): add services configuration

--- supported-version/src/index.ts
+++ supported-version/src/index.ts
@@ -2,20 +2,34 @@ import * as core from '@actions/core';
 import { validateKind } from './kind/validate-kinds';
 import { getMatrixForKind } from './matrix/get-matrix-for-kind';
 import { validateProject } from "./project/validate-projects";
+import { buildServicesForEntry } from "./services/build-services";
 
 
 export async function run(): Promise<void> {
-  try { 
+  try {
     const kind = core.getInput("kind");
     const customVersions = core.getInput("custom_versions");
     const project = core.getInput("project");
-    
+    const include_services = core.getInput("include_services") === "true";
+
     validateProject(<any>project)
 
     validateKind(<any>kind, customVersions ? customVersions.split(',') : undefined);
 
-    core.setOutput('matrix', getMatrixForKind(kind, project, customVersions));
-  } 
+    let matrix = getMatrixForKind(kind, project, customVersions);
+
+    if (include_services) {
+      matrix = {
+        magento: matrix.magento,
+        include: matrix.include.map((entry) => ({
+          ...entry,
+          services: buildServicesForEntry(entry)
+        }))
+      };
+    }
+
+    core.setOutput('matrix', matrix);
+  }
   catch (error) {
     core.setFailed(error.message);
   }
